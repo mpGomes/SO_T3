11ccb232ba7c0a335172d0b69aa2c06bf944e71e
goncalopp <goncalopp@gmail.com>
2011-11-21 19:25:54 +0000

    run_in_foreground.sh

---
+++ run_in_foreground.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+MOUNTPOINT=mountpoint
+DISK=disk.img
+echo "will run in foreground for debug purposes. run the  umount/fusermount -u   from another terminal"
+echo "mounting $DISK in $MOUNTPOINT..."
+./sofs.py -f $MOUNTPOINT -o device=$DISK
+echo "Done!"
